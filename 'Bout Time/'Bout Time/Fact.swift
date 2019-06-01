d5e83f82bbab54d1c38b21e76b5a73ee69fe4706
arwin oblea <arwinoblea@gmail.com>
2019-05-31 21:42:30 -0400

    populated Facts/finished UI

---
+++ 'Bout Time/'Bout Time/Fact.swift
@@ -0,0 +1,15 @@
+//
+//  Fact.swift
+//  'Bout Time
+//
+//  Created by Arwin Oblea on 5/31/19.
+//  Copyright © 2019 Arwin Oblea. All rights reserved.
+//
+
+import Foundation
+
+struct Fact {
+  let title: String
+  let link: String
+  let date: String
+}
